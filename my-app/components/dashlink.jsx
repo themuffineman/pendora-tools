48c1d1ee21402661c28714a2c81981da998c5a95
Petrus <petrusheya@gmail.com>
2024-07-17 22:05:28 +0200

    Minor mods to the dashlinks

--- my-app/components/dashlink.jsx
+++ my-app/components/dashlink.jsx
@@ -8,14 +8,14 @@ import { Accordion , AccordionContent, AccordionItem, AccordionTrigger } from '.
 const Dashlink = ({icon, linkName, url, subUrls}) => { 
   const pathname = usePathname()
   return (
-    <Link href={url} className={`w-full ${pathname !== `/${url}` && 'hover:bg-slate-100'} h-max text-black p-2 rounded-sm flex items-center justify-start gap-2 relative ${pathname === `/${url}`? ' ring-[1px] bg-slate-50 ring-neutral-200 ' : 'bg-white'} `}> 
+    <Link href={url} className={`w-full ${pathname !== `/${url}` && 'hover:bg-slate-100'} h-max text-black p-2 rounded-3xl flex items-center justify-start gap-2 relative ${pathname === `/${url}`? ' ring-[1px] bg-slate-50 ring-neutral-200 ' : 'bg-white'} `}> 
       <Image
         src={`/icons/${icon}`}
         height={15}
         width={15}
         className="object-center"
       />
-      <p className='text-md text-black w-max font-medium'>{linkName}</p> 
+      <p className='text-sm text-black w-max font-medium'>{linkName}</p> 
     </Link>
   )
 }
