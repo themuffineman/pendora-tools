970bde21c74c96f574a7dd28ee42cd0937676f3c
Petrus <petrusheya@gmail.com>
2024-07-24 23:58:59 +0200

    Page edits

--- my-app/app/dashboard/page.jsx
+++ my-app/app/dashboard/page.jsx
@@ -7,12 +7,12 @@ import Chart from '@/components/Chart'
 const page = () => {
   return (
     <div className='flex flex-col items-center justify-start py-14 text-black gap-4 size-full'>
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-20 items-center justify-center  w-full">
         {dashOverviewData.map((data)=>(
           <DashCard title={data.title} icon={data.icon} stat={45} comparison={45}/>
         ))}
       </div>
-      <div className="w-1/2">
+      <div className="w-[80%]">
         <Chart/>
       </div>
       <div></div>
--- my-app/components/DashCard.jsx
+++ my-app/components/DashCard.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const DashCard = ({title, stat, icon, comparison }) => {
   return (
-    <div className='w-[16rem] h-[8rem] text-white bg-neutral-800 hover:ring-1 flex flex-col items-start gap-1 justify-between hover:ring-neutral-900   text-center p-4 rounded-md hover:scale-[5px] transition'>
+    <div className='w-[16rem] h-[8rem] text-white bg-neutral-900 hover:ring-1 flex flex-col items-start gap-1 justify-between hover:ring-neutral-800   text-center p-4 rounded-md hover:scale-[5px] transition'>
         <div className="w-full flex justify-start items-center gap-2">
           <Image
               src={`/icons/${icon}`}
               width={15}
               height={15}
           />
-          <h4 className='text-[12px] font-thin'>{title}</h4>
+          <h4 className='text-[12px] font-medium'>{title}</h4>
         </div>
         <h1 className='text-4xl font-black tracking-tight'>{stat}</h1>
         <div className='flex gap-1 items-center w-max'>
