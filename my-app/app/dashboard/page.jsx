6981bdd260d11e980e914323e67c3a6b9d023f6f
Petrus <petrusheya@gmail.com>
2024-07-25 09:55:30 +0200

    Added Tabs

--- my-app/app/dashboard/page.jsx
+++ my-app/app/dashboard/page.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 import { dashOverviewData } from '../app.config'
 import DashCard from '@/components/DashCard'
 import Chart from '@/components/Chart'
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import styles from '@/components/components.module.css'
 
 const page = () => {
   return (
@@ -11,6 +18,24 @@ const page = () => {
         {dashOverviewData.map((data)=>(
           <DashCard title={data.title} icon={data.icon} stat={45} comparison={45}/>
         ))}
+      </div>
+      <div className="w-full">
+        <Tabs className="w-max bg-black" defaultValue="emailsSent">
+          <TabsList className="grid w-full grid-cols-3 bg-neutral-900">
+            <TabsTrigger value="emailsSent" className={`text-white font-medium ${styles.TabsTrigger}`}>
+              Emails Sent
+            </TabsTrigger>
+            <TabsTrigger value="openRate" className={`text-white font-medium ${styles.TabsTrigger}`}>
+              Open Rate
+            </TabsTrigger>
+            <TabsTrigger value="clickedLinks" className={`text-white font-medium ${styles.TabsTrigger}`}>
+              Clicked Links
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="emailsSent"></TabsContent>
+          <TabsContent value="openRate"></TabsContent>
+          <TabsContent value="clickedLinks"></TabsContent>
+        </Tabs>
       </div>
       <div className="w-[80%]">
         <Chart/>
