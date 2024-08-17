c9e55abd4b1c94607bfdb82c757b7d3d1aae93de
Petrus <petrusheya@gmail.com>
2024-08-17 13:17:02 +0200

    Added scroll area for filter

--- my-app/components/FilterBar.jsx
+++ my-app/components/FilterBar.jsx
@@ -1,18 +1,23 @@
 "use client"
 import React, { useState } from 'react'
 import {BuiltWithFilter} from './Filters'
+import { ScrollArea } from './ui/scroll-area'
+import { Button } from './ui/button'
 
 
 const FilterBar = () => {
     const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className='h-full w-[15rem] flex flex-col items-start overflow-y-auto'>
-        <h2>FilterBar</h2>
+    <div className='h-full flex flex-col items-start justify-between relative'>
+      <h2 className='text-3xl font-extrabold tracking-tight p-3'>Filter</h2>
+      <ScrollArea className='h-max w-[15rem] flex flex-col items-start pb-2  '>
         <BuiltWithFilter/>
         <BuiltWithFilter/>
         <BuiltWithFilter/>
         <BuiltWithFilter/>
         <BuiltWithFilter/>
+      </ScrollArea>
+      <Button className="w-full p-4 hover:bg-black hover:text-white rounded-none bg-yellow-400 text-black">Search</Button>
     </div>
   )
 }
